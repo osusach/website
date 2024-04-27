db994494ac77dee7a0b4f5052a319587b0891f0b
Skyrdow <lucas.mesias@usach.cl>
2024-04-27 14:53:50 -0400

    add: landing page v2

--- src/components/ActiveProject.tsx
+++ src/components/ActiveProject.tsx
@@ -1,24 +1,46 @@
 type Props = {
-	title: string;
-	description: string;
-	href: string;
-	icon: string;
-	iconAlt: string;
+  title: string;
+  description: string;
+  href: string;
+  imageSrc?: string;
+  releaseDate: string;
+  contributors: number;
 };
 
-export default function ActiveProject({ href, title, description, icon, iconAlt }: Props) {
-	return (
-		<div className="hover:bg-neutral-800 border border-white">
-			<a className="p-2 flex flex-col" href={href}>
-				<div className="text-neutral-200 h-12 font-semibold flex flex-row items-center justify-between gap-1">
-					<img src={icon} alt={iconAlt} className="w-6 h-6" />
-					<h3 className="text-center">{title}</h3>
-					<span>&rarr;</span>
-				</div>
-				<p className="text-sm">
-					{description}
-				</p>
-			</a>
-		</div>
-	);
+export default function ActiveProject({
+  href,
+  title,
+  description,
+  imageSrc,
+  releaseDate,
+  contributors,
+}: Props) {
+  const defaultImg = "/logo.png";
+  // Proyectos activos: (grid)
+  // Titulo, link, foto, logo, descripción breve, fecha, cantidad de integrantes en el equipo de desarrollo
+  return (
+    <a
+      className="card overflow-hidden bg-base-300 shadow-[5px_5px_3px_0px_rgba(0,0,0,0.3)]"
+      href={href}
+    >
+      <img
+        src={imageSrc ? imageSrc : defaultImg}
+        alt="Captura de la página"
+        className="mb-2 aspect-video w-full object-cover object-left-top"
+      />
+      <div className="mx-4 flex justify-between">
+        {/* Integrantes */}
+        <span className="badge badge-neutral badge-outline">
+          <img src="/user.svg" alt="" />
+          {contributors}
+        </span>
+        {/* fecha */}
+        <span className="badge badge-neutral badge-outline">{releaseDate}</span>
+      </div>
+      <span className="p-2 pt-0 text-center text-base-content">
+        <h3 className="line-clamp-2 h-14 text-lg font-bold">{title}</h3>
+        <p className="line-clamp-3 text-sm">{description}</p>
+      </span>
+    </a>
+  );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        bumblebee: {
+          ...require("daisyui/src/theming/themes")["bumblebee"],
+          primary: "#da6f2c",
+          "primary-content": "#FFFFFF",
+          secondary: "#da6f2c",
+          "secondary-content": "#FFFFFF",
+          accent: "#da6f2c",
+          "accent-content": "#FFFFFF",
+
+          "base-100": "#fffcf0",
+          "base-200": "#fffef8",
+          "base-300": "#fffffe",
+        },
+      },
+    ],
+  },
+};
